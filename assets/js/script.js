4395d1d527857a5a721d2467575a2883aa32f032
magicjar <9734293+magicjar@users.noreply.github.com>
2020-07-08 15:54:33 +0700

    Some cleaning

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var data = localStorage.darkMode;
+$("body").attr("dark-mode", data);
+
+$('#dark-mode-toggle')
+    .prop('checked', data == 'true')
+    .change(function () {
+        data = $(this).prop('checked');
+        localStorage.darkMode = data;
+        $("body").attr("dark-mode", data);
+    });
+
+$('#markdown-toc').appendTo('#toc');
+
+$(document).on('click', 'a[href]', function (e) {
+    var href = $(this).attr('href');
+
+    if (!href.includes('#')) return;
+
+    var id = '#' + href.split('#').pop();
+    var $id = $(id);
+
+    if ($id.length === 0) return;
+
+    e.preventDefault();
+
+    var pos = $id.offset().top;
+
+    $('body, html').animate({ scrollTop: pos }, 600);
+});
